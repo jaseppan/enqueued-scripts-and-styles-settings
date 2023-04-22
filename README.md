958862ff6081019ef494b25c367d6a834a114b58
Janne Seppänen <j.v.seppanen@gmail.com>
2023-04-22 16:29:03 +0300

    Added for the first version.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# js-wp-optimizer
+# Enqueued Scripts and Styles Editor
+Enqueued Scripts and Styles is a WordPress plugin that allows you to modify how enqueued scripts and styles are loaded on your site. With this plugin, you can select whether to load scripts and styles asynchronously, defer their loading, disable them completely, or leave them as they are.
+
+## Installation
+To install the plugin, follow these steps:
+
+Download the plugin ZIP file from the WordPress plugin repository.
+Upload the ZIP file to your WordPress site by navigating to Plugins > Add New and clicking the "Upload Plugin" button.
+Activate the plugin through the "Plugins" menu in WordPress.
+Usage
+To use the plugin, go to the "Enqueued Scripts and Styles" settings page in the WordPress admin dashboard. On this page, you'll see a list of all enqueued scripts and styles on your site, along with a dropdown menu for each one. Use the dropdown menu to select how the script or style should be loaded: none, async, defer, or disable.
+
+Selected options are saved to the database and are used to modify the enqueued scripts and styles on page load. If the selected option is async or defer, the script will be loaded with the async or defer attribute. If the selected option is disable, the script will be deregistered and will not be loaded at all.
+
+# Contributing
+If you find a bug or have a feature request, please open an issue on the GitHub repository. Pull requests are also welcome!
+
+# License
+Enqueued Scripts and Styles is released under the MIT License.
---
+++ enqueued-scripts-and-styles-settings.php
@@ -0,0 +1,156 @@
+<?php
+
+/**
+ * Plugin Name: Enqueued Scripts and Styles Settings
+ * Plugin URI: https://janneseppanen.site
+ * Description: Allows users to select how enqueued scripts and styles should be loaded.
+ * Version: 1.0
+ * Author: Janne Seppänen
+ * Author URI: https://janneseppanen.site
+ */
+
+// Register the plugin settings page
+add_action( 'admin_menu', 'enqueued_scripts_and_styles_menu' );
+function enqueued_scripts_and_styles_menu() {
+    add_options_page( 'Enqueued Scripts and Styles Settings', 'Enqueued Scripts and Styles', 'manage_options', 'enqueued_scripts_and_styles', 'enqueued_scripts_and_styles_settings_page' );
+}
+
+// Add the plugin settings fields
+add_action( 'admin_init', 'enqueued_scripts_and_styles_settings' );
+function enqueued_scripts_and_styles_settings() {
+    register_setting( 'enqueued_scripts_and_styles_options', 'enqueued_scripts_and_styles_options', 'enqueued_scripts_and_styles_sanitize_options' );
+
+    add_settings_section( 'enqueued_scripts_and_styles_scripts_section', 'Enqueued Scripts', 'enqueued_scripts_and_styles_scripts_section_callback', 'enqueued_scripts_and_styles' );
+    add_settings_section( 'enqueued_scripts_and_styles_styles_section', 'Enqueued Styles', 'enqueued_scripts_and_styles_styles_section_callback', 'enqueued_scripts_and_styles' );
+
+    $registered_scripts = wp_scripts()->registered;
+    $registered_styles = wp_styles()->registered;
+
+    foreach ( $registered_scripts as $handle => $script ) {
+        add_settings_field(
+            'enqueued_scripts_and_styles_scripts_field_' . $handle,
+            $handle,
+            'enqueued_scripts_and_styles_field_callback',
+            'enqueued_scripts_and_styles',
+            'enqueued_scripts_and_styles_scripts_section',
+            array(
+                'type' => 'script',
+                'handle' => $handle,
+                'default' => 'none',
+            )
+        );
+    }
+
+    foreach ( $registered_styles as $handle => $style ) {
+        add_settings_field(
+            'enqueued_scripts_and_styles_styles_field_' . $handle,
+            $handle,
+            'enqueued_scripts_and_styles_field_callback',
+            'enqueued_scripts_and_styles',
+            'enqueued_scripts_and_styles_styles_section',
+            array(
+                'type' => 'style',
+                'handle' => $handle,
+                'default' => 'none',
+            )
+        );
+    }
+}
+
+// Sanitize the plugin options
+function enqueued_scripts_and_styles_sanitize_options( $input ) {
+    $output = array();
+
+    $registered_scripts = wp_scripts()->registered;
+    $registered_styles = wp_styles()->registered;
+
+    foreach ( $registered_scripts as $handle => $script ) {
+        if ( isset( $input[ $handle ] ) && in_array( $input[ $handle ], array( 'none', 'async', 'defer', 'disable' ) ) ) {
+            $output[ $handle ] = $input[ $handle ];
+        } else {
+            $output[ $handle ] = 'none';
+        }
+    }
+
+    foreach ( $registered_styles as $handle => $style ) {
+        if ( isset( $input[ $handle ] ) && in_array( $input[ $handle ], array( 'none', 'async', 'defer', 'disable' ) ) ) {
+            $output[ $handle ] = $input[ $handle ];
+        } else {
+            $output[ $handle ] = 'none';
+        }
+    }
+
+    return $output;
+}
+
+// Display the plugin settings page
+function enqueued_scripts_and_styles_settings_page() {
+    ?>
+    <div class="wrap">
+        <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'enqueued_scripts_and_styles_options' ); ?>
+            <?php do_settings_sections( 'enqueued_scripts_and_styles' ); ?>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
+
+// Display the script settings section header
+function enqueued_scripts_and_styles_scripts_section_callback() {
+    echo '<p>Select how enqueued scripts should be loaded.</p>';
+}
+
+// Display the style settings section header
+function enqueued_scripts_and_styles_styles_section_callback() {
+    echo '<p>Select how enqueued styles should be loaded.</p>';
+}
+
+// Display the plugin settings fields
+function enqueued_scripts_and_styles_field_callback( $args ) {
+    $options = get_option( 'enqueued_scripts_and_styles_options' );
+    $type = $args['type'];
+    $handle = $args['handle'];
+    $default = $args['default'];
+    $selected = isset( $options[ $handle ] ) ? $options[ $handle ] : $default;
+
+    echo '<select name="enqueued_scripts_and_styles_options[' . $handle . ']">';
+    echo '<option value="none" ' . selected( $selected, 'none', false ) . '>None</option>';
+    echo '<option value="async" ' . selected( $selected, 'async', false ) . '>Async</option>';
+    echo '<option value="defer" ' . selected( $selected, 'defer', false ) . '>Defer</option>';
+    echo '<option value="disable" ' . selected( $selected, 'disable', false ) . '>Disable</option>';
+    echo '</select>';
+}
+
+// Modify enqueued scripts and styles based on the plugin options
+add_action( 'wp_enqueue_scripts', 'enqueued_scripts_and_styles_modify_enqueued_scripts_and_styles', 9999 );
+function enqueued_scripts_and_styles_modify_enqueued_scripts_and_styles() {
+    $options = get_option( 'enqueued_scripts_and_styles_options' );
+    $registered_scripts = wp_scripts()->registered;
+    $registered_styles = wp_styles()->registered;
+
+    foreach ( $registered_scripts as $handle => $script ) {
+        if ( isset( $options[ $handle ] ) ) {
+            $option = $options[ $handle ];
+
+            if ( $option === 'async' ) {
+                wp_script_add_data( $handle, 'async', true );
+            } elseif ( $option === 'defer' ) {
+                wp_script_add_data( $handle, 'defer', true );
+            } elseif ( $option === 'disable' ) {
+                wp_deregister_script( $handle );
+            }
+        }
+    }
+
+    foreach ( $registered_styles as $handle => $style ) {
+        if ( isset( $options[ $handle ] ) ) {
+            $option = $options[ $handle ];
+
+            if ( $option === 'disable' ) {
+                wp_deregister_style( $handle );
+            }
+        }
+    }
+}
